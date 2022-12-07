97bedd5c53e372a5438ced772e0e48c148f4102b
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-12-06 21:14:03 -0300

    Implementando teste na função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    read_brazilian = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    for job in read_brazilian:
+        assert job.get("titulo") is None
+        assert job.get("salario") is None
+        assert job.get("tipo") is None
+        assert job.get("title") is not None
+        assert job.get("salary") is not None
+        assert job.get("type") is not None
