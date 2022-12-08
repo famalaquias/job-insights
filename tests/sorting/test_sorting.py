b6d8c815442d3a0ab6a4f49e20e754725930d09c
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-12-08 11:03:37 -0300

    Alterações nos requisitos de testes

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,22 +1,21 @@
 from src.pre_built.sorting import sort_by
 
 
-jobOne = {"min_salary": 100, "max_salary": 1000, "date_posted": "2000-11-19"}
-jobTwo = {"min_salary": "", "max_salary": 3000, "date_posted": "2000-11-19"}
-jobThree = {"min_salary": 300, "max_salary": "", "date_posted": "2000-11-19"}
-jobFour = {"min_salary": 500, "max_salary": 5000, "date_posted": ""}
+job1 = {"min_salary": 100, "max_salary": 1000, "date_posted": '2001-02-01'} 
+job2 = {"min_salary": 50, "max_salary": 2000, "date_posted": ''}
+job3 = {"min_salary": '', "max_salary": 3000, "date_posted":  '2021-02-01'}
+job4 = {"min_salary": 200, "max_salary": '', "date_posted": '2011-02-01'}
 
 
-MOCK_MAX = [jobOne, jobTwo, jobThree, jobFour]
-MOCK_MIN = [jobOne, jobTwo, jobThree, jobFour]
-MOCK_DATE = [jobOne, jobTwo, jobThree, jobFour]
+MOCK_MAX = [job1, job2, job3, job4]
+MOCK_MIN = [job1, job2, job3, job4]
+MOCK_DATE = [job1, job2, job3, job4]
 
 
 def test_sort_by_criteria():
-    pass
     sort_by(MOCK_MAX, "max_salary")
-    assert MOCK_MAX == [jobTwo, jobThree, jobOne, jobFour]
+    assert MOCK_MAX == [job3, job2, job1, job4]
     sort_by(MOCK_MIN, "min_salary")
-    assert MOCK_MIN == [jobThree, jobOne, jobFour, jobTwo]
+    assert MOCK_MIN == [job2, job1, job4, job3]
     sort_by(MOCK_DATE, "date_posted")
-    assert MOCK_DATE == [jobTwo, jobFour, jobOne, jobThree]
+    assert MOCK_DATE == [job3, job4, job1, job2]
